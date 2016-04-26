d8044ce2848cdc766e82ce02c1d8ecadb4747f79
zollenz <dejean75@gmail.com>
2016-04-26 23:02:15 +0200

    Update mint rendering switch

--- src/miosis/ui/MiosisMintRendering.hx
+++ src/miosis/ui/MiosisMintRendering.hx
@@ -14,20 +14,13 @@ import miosis.ui.MiosisSliderRender;
 
 class MiosisMintRendering extends LuxeMintRender 
 {
-    // override function get<T:Control, T1>( _type:Class<T>, _control:T ) : T1 
-    // {
-    //     return cast switch(_type) 
-    //     {
-    //         case Button: new MiosisButtonRender(this, cast _control);
-    //         case _: null;
-    //     }
-    // }
-
-    override function get<T:Control, T1>( type:Class<T>, control:T ) : T1 {
-        return cast switch(type) {
+    override function get<T:Control, T1>( type:Class<T>, control:T ) : T1 
+    {
+        return cast switch(type) 
+        {
             case mint.Canvas:       new mint.render.luxe.Canvas(this, cast control);
             case mint.Label:        new mint.render.luxe.Label(this, cast control);
-            case mint.Button:       new mint.render.luxe.Button(this, cast control);
+            // case mint.Button:       new mint.render.luxe.Button(this, cast control);
             case mint.Image:        new mint.render.luxe.Image(this, cast control);
             case mint.List:         new mint.render.luxe.List(this, cast control);
             case mint.Scroll:       new mint.render.luxe.Scroll(this, cast control);
@@ -39,6 +32,7 @@ class MiosisMintRendering extends LuxeMintRender
             case mint.Slider:       new mint.render.luxe.Slider(this, cast control);
             case mint.Progress:     new mint.render.luxe.Progress(this, cast control);
             // Custom
+            case Button:                    new MiosisButtonRender(this, cast control);            
             case MiosisSliderControl:       new MiosisSliderRender(cast this, cast control);            
             case _:                 null;
         }
